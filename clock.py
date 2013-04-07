2280be37c4e498c06dc479481269634d853bf92b
Chris Fane <chris@fane.cc>
2013-04-07 19:10:30 +0100

    Now with smoother ticks ! tick frequency increased to produce smoother looking ticks.  Now includes start and stop times, as I found it too bright to sleep with it turned on.  Start and stop times represented as python datetimes

--- clock.py
+++ clock.py
@@ -4,6 +4,9 @@ import argparse
 from time import sleep
 from math import sin, pi
 
+stop_time = datetime.time(hour=22, minute=55, second=00)
+start_time = datetime.time(hour=06, minute=00, second=00)
+
 class rgb:
     """ rgb class for storing 3-part rgb colour values """
     def __init__(self, r=0, g=0, b=0):
@@ -17,9 +20,7 @@ class rgb:
 SINE = "sine"
 LINEAR = "linear"
 # Currently supported led drivers
-valid_led_drivers = ['ws2801','lpd6803']
-# Removed lpd8806 until testing complete.
-#valid_led_drivers = ['ws2801','lpd6803','lpd8806']
+valid_led_drivers = ['ws2801','lpd6803','lpd8806']
 
 def interpolate(val, op0, op1, curve=LINEAR):
     """ interpolate an input between two given output values. supports linear and sine curves """
@@ -38,12 +39,12 @@ def calcBinVals(pos):
     rtn = []
 
     bin1 = int(pos)%12
-    val1 = round(interpolate(offset, 255, 0, SINE))
+    val1 = round(interpolate(offset, 240, 0))
     if val1 > 0:
         rtn.append((bin1, val1))
 
     bin2 = int(pos+1)%12
-    val2 = round(interpolate(offset, 0, 255, SINE))
+    val2 = round(interpolate(offset, 0, 240))
     if val2 > 0:
         rtn.append((bin2, val2))
     return rtn
@@ -58,14 +59,25 @@ def getclock(when):
     spos = (when.second+when.microsecond/1000000.0)/5
     
     """ write the rgb values onto the clock face """
+    # If the user has set a stop_time, evaluate our stop and start window.
+    if stop_time is not None:
+        if start_time < when < stop_time:
+            reduction_multiplier = 1
+        else:
+            # if we're outside our run times, set to zero.
+            reduction_multiplier = 0
+    else:
+        # if not stop_time is set just run all the time.
+        reduction_multiplier = 1
+
     for b in calcBinVals(hpos):
-        clock[b[0]].r = b[1]
+        clock[b[0]].r = b[1]*reduction_multiplier
     
     for b in calcBinVals(mpos):
-        clock[b[0]].g = b[1]
+        clock[b[0]].g = b[1]*reduction_multiplier
     
     for b in calcBinVals(spos):
-        clock[b[0]].b = b[1]
+        clock[b[0]].b = b[1]*reduction_multiplier
 
     return clock
 
@@ -95,14 +107,16 @@ if __name__ == "__main__":
     #elif driver_type == 'lpd8806':
     #    from pigredients.ics import lpd8806 as lpd8806
     #    led_chain = lpd8806.LPD8806_Chain(ics_in_chain=12)
-
+    last = datetime.datetime.now()
     while True:
-        now = datetime.datetime.now()
-        clock = getclock(now)
-        #print "time %02d:%02d:%02d.%6d" % (now.hour, now.minute, now.second, now.microsecond)
-        for i in range(len(clock)):
-            #print "Setting led %d, to : %s" % (i, [int(clock[i].r), int(clock[i].g), int(clock[i].b)])
-            led_chain.set_ic(i, [int(clock[i].r), int(clock[i].g), int(clock[i].b*0.7)])
-        led_chain.set()
-        #print [str(c) for c in clock]
-        sleep(1)
+        now = datetime.datetime.now().time()
+        if now.second != last.second:
+            last = now
+            clock = getclock(now)
+            #print "time %02d:%02d:%02d.%6d" % (now.hour, now.minute, now.second, now.microsecond)
+            for i in range(len(clock)):
+                #print "Setting led %d, to : %s" % (i, [int(clock[i].r), int(clock[i].g), int(clock[i].b)])
+                led_chain.set_ic(i, [int(clock[i].r), int(clock[i].g), int(clock[i].b*0.7)])
+            led_chain.set()
+            #print [str(c) for c in clock]
+        sleep(0.1)
