c2f2806fa54e0d3be401c90fd2fd4e62c2ab4eda
Chris Fane <chris@chrisfane.co.uk>
2015-02-21 23:43:27 +0000

    pep8 fixes

--- clock.py
+++ clock.py
@@ -1,4 +1,4 @@
-import datetime 
+import datetime
 import argparse
 
 from time import sleep
@@ -6,6 +6,7 @@ from math import sin, pi
 
 stop_time = datetime.time(hour=22, minute=55, second=00)
 start_time = datetime.time(hour=06, minute=00, second=00)
+
 
 class rgb:
     """ rgb class for storing 3-part rgb colour values """
@@ -20,10 +21,11 @@ class rgb:
 SINE = "sine"
 LINEAR = "linear"
 # Currently supported led drivers
-valid_led_drivers = ['ws2801','lpd6803','lpd8806']
+valid_led_drivers = ['ws2801', 'lpd6803', 'lpd8806']
+
 
 def interpolate(val, op0, op1, curve=LINEAR):
-    """ interpolate an input between two given output values. supports linear and sine curves """
+    # interpolate an input between two given output values. supports linear and sine curves
     diff = op1-op0
     if curve == LINEAR:
         rtn = op0+(diff*val)
@@ -33,32 +35,34 @@ def interpolate(val, op0, op1, curve=LINEAR):
         raise Exception("can't interpolate with a curve of type [%s]" % curve)
     return rtn
 
+
 def calcBinVals(pos):
-    """ helper function to return the bins and bin values for a given position on the clock face """
-    offset = pos%1
+    # helper function to return the bins and bin values for a given position on the clock face
+    offset = pos % 1
     rtn = []
 
-    bin1 = int(pos)%12
+    bin1 = int(pos) % 12
     val1 = round(interpolate(offset, 240, 0))
     if val1 > 0:
         rtn.append((bin1, val1))
 
-    bin2 = int(pos+1)%12
+    bin2 = int(pos + 1) % 12
     val2 = round(interpolate(offset, 0, 240))
     if val2 > 0:
         rtn.append((bin2, val2))
     return rtn
 
+
 def getclock(when):
-    """ build clock face for the given time """
+    # build clock face for the given time
     clock = [rgb() for i in xrange(12)]
-    
-    """ positions of hands on the clock face 0-11 """
-    hpos = (when.hour%12)+when.minute/60.0+when.second/3600
-    mpos = (when.minute+when.second/60.0)/5
-    spos = (when.second+when.microsecond/1000000.0)/5
-    
-    """ write the rgb values onto the clock face """
+
+    # positions of hands on the clock face 0-11
+    hpos = (when.hour % 12) + when.minute / 60.0 + when.second / 3600
+    mpos = (when.minute + when.second / 60.0) / 5
+    spos = (when.second + when.microsecond / 1000000.0) / 5
+
+    # write the rgb values onto the clock face
     # If the user has set a stop_time, evaluate our stop and start window.
     if stop_time is not None:
         if start_time < when < stop_time:
@@ -72,10 +76,10 @@ def getclock(when):
 
     for b in calcBinVals(hpos):
         clock[b[0]].r = b[1]*reduction_multiplier
-    
+
     for b in calcBinVals(mpos):
         clock[b[0]].g = b[1]*reduction_multiplier
-    
+
     for b in calcBinVals(spos):
         clock[b[0]].b = b[1]*reduction_multiplier
 
@@ -104,7 +108,7 @@ if __name__ == "__main__":
     elif driver_type == 'lpd6803':
         from pigredients.ics import lpd6803 as lpd6803
         led_chain = lpd6803.LPD6803_Chain(ics_in_chain=12)
-    #elif driver_type == 'lpd8806':
+    # elif driver_type == 'lpd8806':
     #    from pigredients.ics import lpd8806 as lpd8806
     #    led_chain = lpd8806.LPD8806_Chain(ics_in_chain=12)
     last = datetime.datetime.now()
@@ -113,10 +117,10 @@ if __name__ == "__main__":
         if now.second != last.second:
             last = now
             clock = getclock(now)
-            #print "time %02d:%02d:%02d.%6d" % (now.hour, now.minute, now.second, now.microsecond)
+            # print "time %02d:%02d:%02d.%6d" % (now.hour, now.minute, now.second, now.microsecond)
             for i in range(len(clock)):
-                #print "Setting led %d, to : %s" % (i, [int(clock[i].r), int(clock[i].g), int(clock[i].b)])
-                led_chain.set_ic(i, [int(clock[i].r), int(clock[i].g), int(clock[i].b*0.7)])
+                # print "Setting led %d, to : %s" % (i, [int(clock[i].r), int(clock[i].g), int(clock[i].b)])
+                led_chain.set_ic(i, [int(clock[i].r), int(clock[i].g), int(clock[i].b * 0.7)])
             led_chain.set()
-            #print [str(c) for c in clock]
+            # print [str(c) for c in clock]
         sleep(0.1)
